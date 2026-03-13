f7120fc19eb374c13c7e4b9d5e758d04a01c4c62
KIM.EUNBAE <silvpear897@yonsei.ac.kr>
2026-03-13 05:16:53 +0000

    기본 산술 연산 추가: 합, 차, 곱, 몫, 나머지 출력 기능 구현

--- lec1/lec1_2nd.c
+++ lec1/lec1_2nd.c
@@ -7,6 +7,15 @@ int main(){
     scanf("%d",&num2);
 
     printf("입력한 정수는 각각 [%d] 와/과 [%d] 입니다.\n",num1,num2);
+
+    printf("-------------\n");
+    printf("합 : %d\n", num1 + num2);  //sum
+    printf("차 : %d\n", num1 - num2);  //subtraction
+    printf("곱 : %d\n", num1 * num2); //product
+    printf("몫 : %d\n", num1 / num2); //quotient
+    printf("나머지 : %d\n", num1 % num2); //remainder
+
+    
     return 0;
 
 };
