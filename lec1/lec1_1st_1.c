926455de391b1451f1c83f835f31ae77229de7b1
KIM.EUNBAE <silvpear897@yonsei.ac.kr>
2026-03-13 05:12:02 +0000

    오늘 강의

---
+++ lec1/lec1_1st_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a='A';
+    printf("a %%d: %d\n",a);
+    printf("a %%i: %i\n",a);
+    printf("a %%c: %c\n",a+32);
+    printf("a %%f: %f\n",(float)a);
+    printf("------\n");
+
+
+
+    return 0;
+}
---
+++ lec1/lec1_1st_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int b = -3;
+    printf("b %%d: %d\n",b);
+    printf("b %%i: %i\n",b);
+    printf("b %%c: %c\n",b);
+    printf("b %%f: %f\n",(float)b);
+    printf("b %%u: %u\n",b);
+    printf("-----\n");
+    
+    return 0;
+}
